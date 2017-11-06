606f77734fa285c5b2881d6eae772d4e19d6a11a
Alaaddin30 <Alaaiddin30@gmail.com>
2017-11-06 01:38:44 +0100

    Initial

---
+++ Backend/Backend.DAL/EF/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Backend.DAL.EF
+{
+    public class AppDbContext:IdentityDbContext
+    {
+        public AppDbContext() { }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\\mssqllocaldb;Database=Dating;Trusted_Connection=True;MultipleActiveResultSets=true;",options=>options.EnableRetryOnFailure());
+            }
+        }
+    }
+}
---
+++ Backend/Backend.DAL/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Backend.DAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
